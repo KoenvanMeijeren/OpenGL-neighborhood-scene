7d3ee456751b7b22945047c766bd4de60a7c7236
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-03-22 15:12:27 +0100

    Implement teapot with lightning

--- OpenGLScene/main.cpp
+++ OpenGLScene/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include <GL/glew.h>
 #include <GL/freeglut.h>
@@ -9,10 +10,25 @@
 
 #include "glsl.h"
 #include "objloader.h"
-#include <vector>
 
 using namespace std;
 
+
+//--------------------------------------------------------------------------------
+// Typedefs
+//--------------------------------------------------------------------------------
+struct LightSource
+{
+    glm::vec3 position;
+};
+
+struct Material
+{
+    glm::vec3 ambient_color;
+    glm::vec3 diffuse_color;
+    glm::vec3 specular_color;
+    float power;
+};
 
 //--------------------------------------------------------------------------------
 // Consts
@@ -35,21 +51,22 @@ GLuint program_id;
 GLuint vao;
 
 // Uniform ID's
-GLuint uniform_mvp;
+GLuint uniform_mv;
 
 // Matrices
 glm::mat4 model, view, projection;
-glm::mat4 mvp;
+glm::mat4 mv;
 
+// Light & materials
+LightSource light;
+Material material;
 
 //--------------------------------------------------------------------------------
 // Mesh variables
 //--------------------------------------------------------------------------------
-
 vector<glm::vec3> vertices;
 vector<glm::vec3> normals;
 vector<glm::vec2> uvs;
-
 
 //--------------------------------------------------------------------------------
 // Keyboard handling
@@ -74,13 +91,13 @@ void Render()
 
     // Do transformation
     model = glm::rotate(model, 0.01f, glm::vec3(0.5f, 1.0f, 0.2f));
-    mvp = projection * view * model;
+    mv = view * model;
 
     // Attach to program_id
     glUseProgram(program_id);
 
     // Send mvp
-    glUniformMatrix4fv(uniform_mvp, 1, GL_FALSE, glm::value_ptr(mvp));
+    glUniformMatrix4fv(uniform_mv, 1, GL_FALSE, glm::value_ptr(mv));
 
     // Send vao
     glBindVertexArray(vao);
@@ -121,6 +138,7 @@ void InitGlutGlew(int argc, char** argv)
 
     glEnable(GL_DEPTH_TEST);
     glDisable(GL_CULL_FACE);
+    // glClear(GL_DEPTH_BUFFER_BIT);
 
     glewInit();
 }
@@ -143,16 +161,6 @@ void InitShaders()
 }
 
 //------------------------------------------------------------
-// void InitObjects()
-//------------------------------------------------------------
-
-void InitObjects()
-{
-    bool res;
-    res = loadOBJ("Objects/teapot.obj", vertices, uvs, normals);
-}
-
-//------------------------------------------------------------
 // void InitMatrices()
 //------------------------------------------------------------
 
@@ -167,7 +175,30 @@ void InitMatrices()
         glm::radians(45.0f),
         1.0f * WIDTH / HEIGHT, 0.1f,
         20.0f);
-    mvp = projection * view * model;
+    mv = view * model;
+}
+
+//------------------------------------------------------------
+// void InitMaterialsLight()
+//------------------------------------------------------------
+
+void InitMaterialsLight()
+{
+    light.position = glm::vec3(4.0, 4.0, 4.0);
+    material.ambient_color = glm::vec3(0.2, 0.2, 0.1);
+    material.diffuse_color = glm::vec3(0.5, 0.5, 0.3);
+    material.specular_color = glm::vec3(0.7, 0.7, 0.7);
+    material.power = 1024;
+}
+
+//------------------------------------------------------------
+// void InitObjects()
+//------------------------------------------------------------
+
+void InitObjects()
+{
+    bool res;
+    res = loadOBJ("Objects/teapot.obj", vertices, uvs, normals);
 }
 
 
@@ -179,7 +210,9 @@ void InitMatrices()
 void InitBuffers()
 {
     GLuint position_id;
+    GLuint normal_id;
     GLuint vbo_vertices;
+    GLuint vbo_normals;
 
     // vbo for vertices
     glGenBuffers(1, &vbo_vertices);
@@ -187,8 +220,15 @@ void InitBuffers()
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+    // vbo for normals
+    glGenBuffers(1, &vbo_normals);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_normals);
+    glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals[0], GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     // Get vertex attributes
     position_id = glGetAttribLocation(program_id, "position");
+    normal_id = glGetAttribLocation(program_id, "normal");
 
     // Allocate memory for vao
     glGenVertexArrays(1, &vao);
@@ -202,11 +242,35 @@ void InitBuffers()
     glEnableVertexAttribArray(position_id);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+    // Bind normals to vao
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_normals);
+    glVertexAttribPointer(normal_id, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(normal_id);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     // Stop bind to vao
     glBindVertexArray(0);
 
     // Make uniform vars
-    uniform_mvp = glGetUniformLocation(program_id, "mvp");
+    uniform_mv = glGetUniformLocation(program_id, "mv");
+    GLuint uniform_projection = glGetUniformLocation(program_id, "projection");
+    GLuint uniform_light_pos = glGetUniformLocation(program_id, "light_pos");
+    GLuint uniform_material_ambient = glGetUniformLocation(program_id, "mat_ambient");
+    GLuint uniform_material_diffuse = glGetUniformLocation(program_id, "mat_diffuse");
+    GLuint uniform_material_specular = glGetUniformLocation(program_id, "mat_specular");
+    GLuint uniform_material_power = glGetUniformLocation(program_id, "mat_power");
+
+    // Attach to program
+    glUseProgram(program_id);
+
+    // Fill uniform vars
+    glUniformMatrix4fv(uniform_mv, 1, GL_FALSE, glm::value_ptr(mv));
+    glUniformMatrix4fv(uniform_projection, 1, GL_FALSE, glm::value_ptr(projection));
+    glUniform3fv(uniform_light_pos, 1, glm::value_ptr(light.position));
+    glUniform3fv(uniform_material_ambient, 1, glm::value_ptr(material.ambient_color));
+    glUniform3fv(uniform_material_diffuse, 1, glm::value_ptr(material.diffuse_color));
+    glUniform3fv(uniform_material_specular, 1, glm::value_ptr(material.specular_color));
+    glUniform1f(uniform_material_power, material.power);
 }
 
 
@@ -216,6 +280,7 @@ int main(int argc, char** argv)
     InitShaders();
     InitMatrices();
     InitObjects();
+    InitMaterialsLight();
     InitBuffers();
 
     // Hide console window
