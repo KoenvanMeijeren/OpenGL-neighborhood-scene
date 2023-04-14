b1505ad79379a88659dc08c7a2028bb156513f30
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-04-14 09:27:46 +0200

    Add more objects

--- OpenGLScene/scene_manager.cpp
+++ OpenGLScene/scene_manager.cpp
@@ -6,6 +6,7 @@
 #include "camera.h"
 #include "glsl.h"
 #include "material_default.h"
+#include "material_lambert.h"
 
 scene_manager::scene_manager()
 {
@@ -37,6 +38,19 @@ void scene_manager::init()
     teapot->init_buffers();
     teapot->add_animation(new animation_rotate(0.01f, 0.5f, 1.0f, 0.2f));
 
+    const auto teapot1_position = glm::vec3(0, 5, 0);
+	const auto teapot1_light_position = glm::vec3(4.0, 4.0, 4.0);
+    material* teapot1_material = new material_default(
+        glm::vec3(0.0, 0.0, 0.0), 
+        glm::vec3(0.0, 0.0, 0.0), 
+        glm::vec3(1), 
+        1
+    );
+    auto *teapot1 = new object(teapot1_position, teapot1_light_position, teapot1_material);
+    teapot1->set_object("Objects/teapot.obj");
+    teapot1->set_texture("Textures/Yellobrk.bmp");
+    teapot1->init_buffers();
+
     const auto torus_position = glm::vec3(3.0, 2.5, 0);
 	const auto torus_light_position = glm::vec3(4.0, 4.0, 4.0);
     material* torus_material = new material_default(
@@ -64,9 +78,22 @@ void scene_manager::init()
     box->init_buffers();
     box->add_animation(new animation_rotate(0.05f, 1.0f, 0, 0));
 
+    const auto box2_position = glm::vec3(-3.0, 2.5, 0);
+	const auto box2_light_position = glm::vec3(4.0, 4.0, 4.0);
+    material* box_material_lambert = new material_lambert(
+        glm::vec3(0.3, 0.3, 0.0), 
+        glm::vec3(0.5, 0, 0.5), 
+        glm::vec3(1)
+    );
+    auto *box2 = new object(box2_position, box2_light_position, box_material_lambert);
+    box2->set_object("Objects/box.obj");
+    box2->init_buffers();
+
     objects_.push_back(teapot);
+    objects_.push_back(teapot1);
     objects_.push_back(torus);
     objects_.push_back(box);
+    objects_.push_back(box2);
 }
 
 void scene_manager::render() const
