6e45e442bc645b3c5837e551143c20d7bf00fe42
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-04-15 16:08:19 +0200

    Implement grass floor

--- OpenGLScene/floor-builder.cpp
+++ OpenGLScene/floor-builder.cpp
@@ -8,10 +8,10 @@ void floor_builder::init()
 	const auto square_position = glm::vec3(0, 0, 0);
 	const auto square_light_position = glm::vec3(1.0, 1.0, 1.0);
     material* square_material_metal = new material_default(
-        glm::vec3(0.3, 10, 0), 
-        glm::vec3(0, 5, 0), 
-        glm::vec3(1),
-        2048
+        glm::vec3(0, 0.5, 0), 
+        glm::vec3(0, 0.5, 0), 
+        glm::vec3(0),
+        1024
     );
 
 	for (int position_x = -10; position_x <= 10; ++position_x)
@@ -21,7 +21,8 @@ void floor_builder::init()
 			auto *square = new object_square(
 		        square_position,
 		        square_light_position, 
-		        square_material_metal
+		        square_material_metal,
+				"Textures/grass.bmp"
 		    );
 		    square->translate(static_cast<float>(position_x * 2), 0, static_cast<float>(position_z * 2));
 		    objects_.push_back(square);
